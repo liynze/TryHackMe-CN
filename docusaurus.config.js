2ac3f993161c1c8f2d158a15e1823ba2d92df254
Randark <randark@foxmail.com>
2023-12-10 16:41:25 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://Tryhackme-CN.CTF-Archives.github.io',
+    url: 'https://ctf-archives.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/Tryhackme-CN',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
